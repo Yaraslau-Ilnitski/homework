56155e3e55fd44b8f779ffc79555a5b72ecf9cfe
Yaraslau <yilnitski@gmail.com>
2021-02-18 20:32:41 +0300

    Лщл

--- HM11.py
+++ HM11.py
@@ -3,4 +3,4 @@ b = float(input('Введите число b: '))
 summ = a + b
 dif = a - b
 multi = a * b
-print(' Сумма =', summ, ' Разность =', dif, ' Произведение =', multi)
+print(' Сумммма =', summ, ' Разность =', dif, ' Произведение =', multi)
