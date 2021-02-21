5acbc7a9a26346e95a6d38f37551429e03343330
Yaraslau <yilnitski@gmail.com>
2021-02-21 05:35:21 +0300

    bla

--- Homework1/task_1_5.py
+++ Homework1/task_1_5.py
@@ -1,6 +1,7 @@
 import math
+
 a = float(input('Введите число a: '))
 b = float(input('Введите число b: '))
 c = math.sqrt(a * a + b * b)
-S = (a * b)/2
+S = (a * b) / 2
 print('Гипотенуза =', c, 'Площадь =', S)
